ba65a752cf9e25951a7892c76a0d9184d5dae6c3
Miquéias Araújo <57195879+MiqSA@users.noreply.github.com>
2023-06-26 18:00:31 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
-function Home() { 
-    return (
-        <h1>Lia te amoooo! Você é a maior benção, o maior milagre e o maior desafio que Deus me deu.</h1>
-    )
+function Home() {
+  return (
+    <h1>
+      Lia te amoooo! Você é a maior benção, o maior milagre e o maior desafio
+      que Deus me deu.
+    </h1>
+  );
 }
 
+function teste() {
+  console.log("aqui");
+}
 export default Home;
